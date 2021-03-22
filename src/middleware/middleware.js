e9657fb9813b12d49f8f56f24a7283a2d7899629
Nikhil kumar <nikhil.kumar@moveinsync.com>
2021-03-22 23:04:03 +0530

    adding pics and response type

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -4,7 +4,8 @@ const checkInCache = async (ctx, next) => {
     //if key found in redis then response will return from redis
     try {
         let response = await redisClient.get('comments');
-        if (response) return ctx.body = response;
+        console.log('response ',response)
+        if (response) return ctx.body = await JSON.parse(response);
         await next();
     } catch (error) {
         await next();
